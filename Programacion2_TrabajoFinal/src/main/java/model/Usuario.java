8132fb11a37b8640d49c35a4c6864ab08380c1ee
jorge <jorgelizarraga1994@gmail.com>
2025-05-17 15:31:54 -0300

    commit main jorge

--- Programacion2_TrabajoFinal/src/main/java/model/Usuario.java
+++ Programacion2_TrabajoFinal/src/main/java/model/Usuario.java
@@ -1,4 +1,11 @@
 package model;
 
+import java.util.List;
+
 public class Usuario {
+    String nombreUSuario;
+    String nombre;
+    String apellido;
+    String contrasenia;
+    List<Entrada> entradas;
 }
