1a56f65b39cdf45d2074d837b8b2df333d548fbb
jorge <jorgelizarraga1994@gmail.com>
2025-05-17 16:05:02 -0300

    commit jorge

--- src/main/java/model/Entrada.java
+++ src/main/java/model/Entrada.java
@@ -2,5 +2,7 @@ package model;
 
 public class Entrada {
     private String id;
+    private String codigoEspect;
+    private String sede;
     //System.out.println("Hello world!");
 }
--- src/main/java/model/Usuario.java
+++ src/main/java/model/Usuario.java
@@ -1,4 +1,11 @@
 package model;
 
+import java.util.List;
+
 public class Usuario {
+    private String nombreUsuario;
+    private String nombre;
+    private String apellido;
+    private String contrasenia;
+    private List<Entrada> entradas;
 }
